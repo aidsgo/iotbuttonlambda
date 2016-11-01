949d143b4b8b4603bc8ad5a2b3080184fbf50520
Dong Chen <chen_dong85@foxmail.com>
2016-11-01 14:20:58 +0800

    add the endpoint to notify the server

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ exports.handler = (event, context) => {
     console.log('Received event:', event.clickType);
 
     const options = {
-        url: '',
+        url: 'https://tranquil-peak-57296.herokuapp.com/emergency',
         json: true,
         method:'POST',
         maxAttempts: 3,
         retryDelay: 100,
         retryStrategy: request.RetryStrategies.HTTPOrNetworkError,
         body:{
-            "deviceId":event.serialNumber
+            "serial_number":event.serialNumber
         }
     };
 
     request(options)
         .then(response => {
             if (response.statusCode === 200) {
+                console.log(response.body);
                 console.log("sent the request. Id is:",event.serialNumber);
             } else {
                 throw response.statusCode;
